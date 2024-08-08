337a0f573eded3a9bcd86951ecf643b2a406cb83
dangvu2408 <hiend6887@gmail.com>
2024-08-08 15:09:51 +0700

    [COMMIT - 04] wALLet App Project [layout/home_fragment (layout)] (Aug 8, 2024)

--- app/src/main/java/com/example/walletapp/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/walletapp/Fragment/HomeFragment.java
@@ -1,5 +1,11 @@
 package com.example.walletapp.Fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.MaskFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +13,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.walletapp.R;
@@ -18,6 +25,17 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
+        CardView cardView = view.findViewById(R.id.layout_root);
+
+//        cardView.setDrawingCacheEnabled(true);
+//        Bitmap bitmap = Bitmap.createBitmap(cardView.getDrawingCache());
+//        cardView.setDrawingCacheEnabled(false);
+//        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+//        Canvas canvas = new Canvas(output);
+//        Paint paint = new Paint();
+//        paint.setMaskFilter(new BlurMaskFilter(25, BlurMaskFilter.Blur.NORMAL));
+//        canvas.drawBitmap(bitmap, 0, 0, paint);
+//        cardView.setBackground(new BitmapDrawable(getResources(), output));
         return view;
     }
 }
