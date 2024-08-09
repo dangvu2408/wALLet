9fee1e338ea254ddebd5233966abfe23de121803
dangvu2408 <hiend6887@gmail.com>
2024-08-10 00:02:59 +0700

    [COMMIT - 06] wALLet App Project [layout/splash activity (logo gif) ] (Aug 8, 2024)

---
+++ app/src/main/java/com/example/walletapp/Activity/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.walletapp.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.walletapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
