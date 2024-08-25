c0bdffd602825b8ed8c2bc76ada44fe982ba996e
dangvu2408 <hiend6887@gmail.com>
2024-08-26 01:00:54 +0700

    [COMMIT - 36] wALLet App Project [OTP - simple02] (Aug 25, 2024)

--- app/src/main/java/com/example/walletapp/Activity/OTPActivity.java
+++ app/src/main/java/com/example/walletapp/Activity/OTPActivity.java
@@ -1,16 +1,58 @@
 package com.example.walletapp.Activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.walletapp.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class OTPActivity extends AppCompatActivity {
+    private TextView countdown_time_otp, resend_otp;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_input);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.color_theme_1));
+        }
+        countdown_time_otp = findViewById(R.id.countdown_time_otp);
+        resend_otp = findViewById(R.id.resend_otp);
+        new CountDownTimer(20*600, 1000) {
+            public void onTick(long millisUntilFinished) {
+                countdown_time_otp.setText("Bạn có thể yêu cầu gửi mã mới sau " + new SimpleDateFormat("mm:ss").format(new Date(millisUntilFinished)));
+                resend_otp.setOnClickListener(null);
+            }
+            public void onFinish() {
+                countdown_time_otp.setText("Bạn đã có thể yêu cầu gửi mã mới");
+                resend_otp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new CountDownTimer(20*600, 1000) {
+                            public void onTick(long millisUntilFinished) {
+                                countdown_time_otp.setText("Bạn có thể yêu cầu gửi mã mới sau " + new SimpleDateFormat("mm:ss").format(new Date(millisUntilFinished)));
+                            }
+                            public void onFinish() {
+                                countdown_time_otp.setText("Bạn đã có thể yêu cầu gửi mã mới");
+                            }
+                        }.start();
+                    }
+                });
+            }
+        }.start();
+
+
     }
 }
--- app/src/main/java/com/example/walletapp/Activity/PhoneNumberActivity.java
+++ app/src/main/java/com/example/walletapp/Activity/PhoneNumberActivity.java
@@ -1,12 +1,16 @@
 package com.example.walletapp.Activity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.walletapp.R;
 
@@ -18,6 +22,12 @@ public class PhoneNumberActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_new_account);
         back_to_main = findViewById(R.id.back_to_main);
         next_to_otp_btn = findViewById(R.id.next_to_otp_btn);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.color_theme_1));
+        }
 
 
         back_to_main.setOnClickListener(new View.OnClickListener() {
