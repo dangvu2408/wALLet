ac47d4bfd2747a9e1a7da0edc3b8fff4f473ab2a
dangvu2408 <hiend6887@gmail.com>
2024-08-28 18:28:15 +0700

    [COMMIT - 45] wALLet App Project [mySQL database] (Aug 28, 2024)

--- app/src/main/java/com/example/walletapp/Activity/LoginActivity.java
+++ app/src/main/java/com/example/walletapp/Activity/LoginActivity.java
@@ -4,29 +4,51 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.walletapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-    private LinearLayout login_btn;
+    private LinearLayout login_btn, fill_username_now, fill_pass_now;
     private TextView create_account;
     private SQLiteDatabase database;
+    private EditText username_input;
+    private TextInputEditText password_input;
     private String SRC_DATABASE_NAME = "app_database.db";
+    private String BASE_URL = "http://192.168.1.41/wALLet_app/login.php";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         login_btn = findViewById(R.id.login_btn);
         create_account = findViewById(R.id.create_account);
+        username_input = findViewById(R.id.username_input);
+        password_input = findViewById(R.id.password_input);
+        fill_username_now = findViewById(R.id.fill_username_now);
+        fill_pass_now = findViewById(R.id.fill_pass_now);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
@@ -39,8 +61,18 @@ public class LoginActivity extends AppCompatActivity {
                 String src = getDatabasePath(SRC_DATABASE_NAME).getAbsolutePath();
                 database = SQLiteDatabase.openOrCreateDatabase(src, null);
                 database.delete("userdata", null, null);
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                overridePendingTransition(R.anim.zoom_out, R.anim.zoom_in);
+                String user = username_input.getText().toString().trim();
+                String pass = password_input.getText().toString().trim();
+                if (user.equals("")) {
+                    fill_username_now.setVisibility(View.VISIBLE);
+                } else if (pass.equals("")) {
+                    fill_pass_now.setVisibility(View.VISIBLE);
+                    fill_username_now.setVisibility(View.GONE);
+                } else {
+                    fill_username_now.setVisibility(View.GONE);
+                    fill_pass_now.setVisibility(View.GONE);
+                    login(BASE_URL, user, pass);
+                }
             }
         });
 
@@ -57,6 +89,40 @@ public class LoginActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(0, 0);
+    }
+
+    private void login(String url, String str1, String str2) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest strRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.trim().equals("Success")) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            overridePendingTransition(R.anim.zoom_out, R.anim.zoom_in);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại, vui lòng thử lại!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d("DEBUG", "SERVER ERROR");
+                    }
+                }
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("usernameSignin", str1);
+                params.put("passwordSignin", str2);
+                return params;
+            }
+        };
+        queue.add(strRequest);
     }
 
 }
--- app/src/main/java/com/example/walletapp/Activity/PhoneNumberActivity.java
+++ app/src/main/java/com/example/walletapp/Activity/PhoneNumberActivity.java
@@ -18,13 +18,24 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.walletapp.R;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class PhoneNumberActivity extends AppCompatActivity {
-    private LinearLayout back_to_main, next_to_otp_btn, note_fill;
+    private LinearLayout back_to_main, next_to_otp_btn, note_fill, note_already_exits;
     private EditText phone_number_input;
+    private String BASE_URL = "http://192.168.1.41/wALLet_app/phonenumExits.php";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +43,7 @@ public class PhoneNumberActivity extends AppCompatActivity {
         back_to_main = findViewById(R.id.back_to_main);
         next_to_otp_btn = findViewById(R.id.next_to_otp_btn);
         phone_number_input = findViewById(R.id.phone_number_input);
+        note_already_exits = findViewById(R.id.note_already_exits);
         note_fill = findViewById(R.id.note_fill);
         note_fill.setVisibility(View.GONE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -66,23 +78,16 @@ public class PhoneNumberActivity extends AppCompatActivity {
                         @Override
                         public void onClick(View v) {
                             String phone_num = phone_number_input.getText().toString();
-                            Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
-                            intent.putExtra("key_phone_number", phone_num);
-                            startActivity(intent);
-                            overridePendingTransition(0, 0);
+                            checkPhoneNum(BASE_URL, phone_num);
                         }
                     });
                 }
             }
-
             @Override
             public void afterTextChanged(Editable s) {
 
             }
         });
-
-        String phone_num = phone_number_input.getText().toString();
-
     }
 
 
@@ -90,6 +95,41 @@ public class PhoneNumberActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(0, 0);
+    }
+
+    private void checkPhoneNum(String url, String phone) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.trim().equals("Already")) {
+                            note_already_exits.setVisibility(View.VISIBLE);
+                        } else {
+                            note_already_exits.setVisibility(View.GONE);
+                            Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
+                            intent.putExtra("key_phone_number", phone);
+                            startActivity(intent);
+                            overridePendingTransition(0, 0);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(PhoneNumberActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d("DEBUG", "SERVER ERROR");
+                    }
+                }
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("usernameSignin", phone);
+                return params;
+            }
+        };
+        queue.add(stringRequest);
     }
 
 }
