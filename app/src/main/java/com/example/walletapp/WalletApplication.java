909f33558893d957c56e86708f86d6334c0b7216
dangvu2408 <hiend6887@gmail.com>
2024-09-11 23:34:45 +0700

    [OUTLINE COMMIT (77)] wALLet App Project [login session] (Sep 11 2024)

--- app/src/main/java/com/example/walletapp/WalletApplication.java
+++ app/src/main/java/com/example/walletapp/WalletApplication.java
@@ -3,16 +3,21 @@ package com.example.walletapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Application;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.walletapp.Activity.LoginActivity;
+
 public class WalletApplication extends Application {
     private Handler handler = new Handler();
     private Runnable logoutRunnable;
     private AlertDialog logoutDialog;
+    private static final long TIMEOUT = 1 * 60 * 1000;
 
     @Override
     public void onCreate() {
@@ -30,12 +35,12 @@ public class WalletApplication extends Application {
 
             @Override
             public void onActivityResumed(@NonNull Activity activity) {
-
+                resetUserInactivityHandler(activity);
             }
 
             @Override
             public void onActivityPaused(@NonNull Activity activity) {
-
+                stopUserInactivityHandler();
             }
 
             @Override
@@ -53,5 +58,48 @@ public class WalletApplication extends Application {
 
             }
         });
+    }
+
+    private void resetUserInactivityHandler(Activity currentActivity) {
+        stopUserInactivityHandler();
+        logoutRunnable = new Runnable() {
+            @Override
+            public void run() {
+                showLogoutDialog(currentActivity);
+            }
+        };
+        handler.postDelayed(logoutRunnable, TIMEOUT);
+    }
+
+    private void stopUserInactivityHandler() {
+        handler.removeCallbacks(logoutRunnable);
+        if (logoutDialog != null && logoutDialog.isShowing()) {
+            logoutDialog.dismiss(); // Đóng dialog nếu đang hiển thị
+        }
+    }
+
+    private void showLogoutDialog(Activity currentActivity) {
+        if (logoutDialog != null && logoutDialog.isShowing()) return;
+
+        // Tạo AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(currentActivity);
+        builder.setTitle("Đã đăng xuất")
+                .setMessage("Bạn đã bị đăng xuất do không hoạt động. Vui lòng đăng nhập lại.")
+                .setCancelable(false)
+                .setPositiveButton("Đăng nhập lại", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        forceLogout(currentActivity);
+                    }
+                });
+
+        logoutDialog = builder.create();
+        logoutDialog.show();
+    }
+
+    private void forceLogout(Activity currentActivity) {
+        Intent intent = new Intent(currentActivity, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
