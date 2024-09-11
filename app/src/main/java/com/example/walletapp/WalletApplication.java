50b44970da601c4e83e6b767db2a69c39b844829
dangvu2408 <hiend6887@gmail.com>
2024-09-12 00:26:40 +0700

    [OUTLINE COMMIT (78)] wALLet App Project [login session] (Sep 11 2024)

--- app/src/main/java/com/example/walletapp/WalletApplication.java
+++ app/src/main/java/com/example/walletapp/WalletApplication.java
@@ -7,11 +7,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.walletapp.Activity.LoginActivity;
+import com.example.walletapp.Activity.PasswordActivity;
 
 public class WalletApplication extends Application {
     private Handler handler = new Handler();
@@ -74,32 +79,47 @@ public class WalletApplication extends Application {
     private void stopUserInactivityHandler() {
         handler.removeCallbacks(logoutRunnable);
         if (logoutDialog != null && logoutDialog.isShowing()) {
-            logoutDialog.dismiss(); // Đóng dialog nếu đang hiển thị
+            logoutDialog.dismiss();
         }
     }
 
     private void showLogoutDialog(Activity currentActivity) {
+        if (currentActivity instanceof LoginActivity) return;
         if (logoutDialog != null && logoutDialog.isShowing()) return;
 
-        // Tạo AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(currentActivity);
-        builder.setTitle("Đã đăng xuất")
-                .setMessage("Bạn đã bị đăng xuất do không hoạt động. Vui lòng đăng nhập lại.")
-                .setCancelable(false)
-                .setPositiveButton("Đăng nhập lại", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        forceLogout(currentActivity);
-                    }
-                });
-
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialog = inflater.inflate(R.layout.dialog_logout_timeout, null);
+        builder.setView(dialog);
+        builder.setCancelable(false);
         logoutDialog = builder.create();
-        logoutDialog.show();
+
+        if (currentActivity != null && !currentActivity.isFinishing()) {
+            logoutDialog.getWindow().setBackgroundDrawableResource(R.drawable.transparent_background);
+            ImageView close_dialog = dialog.findViewById(R.id.close_dialog);
+            Button back_to_login_btn = dialog.findViewById(R.id.back_to_login_btn);
+
+            close_dialog.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    forceLogout(currentActivity);
+                }
+            });
+            back_to_login_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    forceLogout(currentActivity);
+                }
+            });
+
+            logoutDialog.show();
+        }
     }
 
     private void forceLogout(Activity currentActivity) {
         Intent intent = new Intent(currentActivity, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+        currentActivity.overridePendingTransition(R.anim.zoom_out, R.anim.zoom_in);
     }
 }
