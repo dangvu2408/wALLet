78e82b7766f174765d7e0e1b9eb00f443a90e198
dangvu2408 <hiend6887@gmail.com>
2024-09-17 23:16:34 +0700

    [OUTLINE COMMIT (86)] wALLet App Project [login setting 1] (Sep 17 2024)

--- app/src/main/java/com/example/walletapp/Activity/LoginSettingActivity.java
+++ app/src/main/java/com/example/walletapp/Activity/LoginSettingActivity.java
@@ -1,16 +1,41 @@
 package com.example.walletapp.Activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.walletapp.R;
 
 public class LoginSettingActivity extends AppCompatActivity {
+    private ImageView back_btn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.color_theme_2));
+        }
         setContentView(R.layout.activity_login_setting);
+        back_btn = findViewById(R.id.back_btn);
+
+
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.close_in, R.anim.close_out);
+            }
+        });
     }
 }
